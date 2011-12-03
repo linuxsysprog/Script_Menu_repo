dc0257109dcc4c87ab7e637eee651f58bd9da72c
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-03 05:48:34 +0000

    added AddRuler skeleton files
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@10 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

---
+++ Application Extensions/AddRuler.cs
@@ -0,0 +1,42 @@
+// File: AddRuler.cs - Insert a frame with a ruler at the top or bottom
+
+using System;
+using System.Drawing;
+using System.Collections;
+using System.Windows.Forms;
+using Sony.Vegas;
+
+public class AddRuler : ICustomCommandModule {
+	private Vegas vegas;
+	private CustomCommand addRulerCmd = new CustomCommand(CommandCategory.View, "AddRulerCmd");
+	
+	public void InitializeModule(Vegas vegas) {
+		this.vegas = vegas;
+	}
+
+	public ICollection GetCustomCommands() {
+		addRulerCmd.DisplayName = "Add Ruler View";
+		addRulerCmd.Invoked += this.HandleInvoked;
+		addRulerCmd.MenuPopup += this.HandleMenuPopup;
+		return new CustomCommand[] { addRulerCmd };
+	}
+
+	void HandleInvoked(Object sender, EventArgs args) {
+		if (!vegas.ActivateDockView("AddRulerView")) {
+			DockableControl addRulerView = new DockableControl("Add Ruler");
+			
+			// create a label
+			Label label = new Label();
+			label.Dock = DockStyle.Fill;
+			label.Text = "Under Construction";
+			label.TextAlign = ContentAlignment.MiddleCenter;
+			addRulerView.Controls.Add(label);
+			
+			vegas.LoadDockView(addRulerView);
+		}
+	}
+
+	void HandleMenuPopup(Object sender, EventArgs args) {
+		addRulerCmd.Checked = vegas.FindDockView("Add Ruler");
+	}
+}
