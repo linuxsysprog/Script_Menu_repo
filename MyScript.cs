782546c9d20793d96ee8d96a958aaf2d04236c26
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-03 03:09:32 +0000

    tried debug prints
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@8 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- MyScript.cs
+++ MyScript.cs
@@ -6,6 +6,9 @@ using UtilityMethods;
 
 public class EntryPoint {
     public void FromVegas(Vegas vegas) {
+		// 
+		System.Diagnostics.Debug.WriteLine("elmo FromVegas() Entry.");
+		
         MessageBox.Show("" + AddClass.Add(2, 3));
         MessageBox.Show(Sony.Vegas.Script.File);
     }
