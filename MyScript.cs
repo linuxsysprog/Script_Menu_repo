c6fc8b205ca4434b136e5b2ab17ebdd27e6df23a
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-17 05:02:07 +0000

    an example of how-to get selectedAudioTracks
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@29 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- MyScript.cs
+++ MyScript.cs
@@ -9,21 +9,8 @@ using AddRuler;
 
 public class EntryPoint {
     public void FromVegas(Vegas vegas) {
-		List<VideoTrack> videoTracks = Video.FindVideoTracks(vegas.Project);
-		// List<Track> tracks = (List<Track>)audioTracks;
-		List<Track> tracks = Common.VideoTracksToTracks(videoTracks);
-		
-		List<VideoTrack> videoTracks2 = Common.TracksToVideoTracks(tracks);
-		MessageBox.Show("" + videoTracks2[0].CompositeLevel);
-	
-		// List<AudioTrack> audioTracks = Audio.FindAudioTracks(vegas.Project);
-		// List<Track> tracks = audioTracks.ConvertAll(new Converter<AudioTrack, Track>(AudioTrackToTrack));
-		// List<Track> selectedTracks = Common.FindSelectedTracks(tracks);
-		// MessageBox.Show("" + selectedTracks.Count);
+		List<AudioTrack> selectedAudioTracks = Common.TracksToAudioTracks(Common.FindSelectedTracks(Common.AudioTracksToTracks(Audio.FindAudioTracks(vegas.Project))));
+		MessageBox.Show("" + selectedAudioTracks.Count);
     }
 	
-	public static Track AudioTrackToTrack(AudioTrack audioTrack) {
-		return (Track)audioTrack;
-	}
-
 }
