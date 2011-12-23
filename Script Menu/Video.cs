17132eb1b3655c2809d4bc1a8c58a0b76ded338c
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-23 04:56:02 +0000

    do add ruler
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@50 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/Video.cs
+++ Script Menu/Video.cs
@@ -25,13 +25,19 @@ public class Video {
 	// add a ruler 1 frame long onto the track specified at the position specified
 	public static VideoEvent AddRuler(VideoTrack videoTrack, Timecode position,
 		bool top, int number, string notes) {
+		// Common.vegas.DebugOut("is videoTrack null = " + (videoTrack == null) + "\n" +
+				// "is videoTrack null = " + (videoTrack == null) + "\n" +
+				// "is position null = " + (position == null) + "\n" +
+				// "top = " + top + "\n" +
+				// "number = " + number + "\n" +
+				// "notes = " + notes);
 		
 		// Calling Media() constructor from an Application Extension like this causes Vegas to crash.
 		// It works, however, within a regular script. Here, we'll use MediaPool to work around the problem.
 		// Media media = new Media(Common.vegas.InstallationDirectory + "\\Application Extensions\\AddRuler.png\\" +
 			// Common.LocationNumber2Basename(top, number));
 		
-		string path = Common.vegas.InstallationDirectory + "\\Application Extensions\\AddRuler.png\\" +
+		string path = Common.vegas.InstallationDirectory + "\\Script Menu\\AddRuler.png\\" +
 			Common.LocationNumber2Basename(top, number);
 
 		// Oh my, this crashes Vegas also. Ok, our ultimate weapon is gonna be the vegas.OpenFile()!!
@@ -63,7 +69,7 @@ public class Video {
 		
 		// All right, everything has failed so far. I'm afraid we need to go back
 		// from Application Extensions to normal scripts.
-		Media media = Common.vegas.Project.MediaPool.Find(path);
+		Media media = new Media(path);
 		
 		VideoEvent videoEvent = videoTrack.AddVideoEvent(position, Timecode.FromFrames(1));
 		
