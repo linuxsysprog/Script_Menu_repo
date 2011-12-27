87f3417a4230dd895ff670ff105cbbe718f63cd8
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-27 02:29:33 +0000

    GUI finished for AddBeep
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@66 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/AddBeep.cs
+++ Script Menu/AddBeep.cs
@@ -9,83 +9,59 @@ using Sony.Vegas;
 using AddRulerNamespace;
 
 public class EntryPoint : Form {
-	private object[] TOP_RANGE = new object[] { "1", "2", "3", "4", "5",
-		"6", "7", "8", "9", "10",
-		"11", "12" };
-
-	private object[] BOTTOM_RANGE = new object[] { "1", "2", "3", "4", "5",
-		"6", "7", "8", "9", "10",
-		"11", "12", "13", "14", "15",
-		"16" };
-
-	private GroupBox gbLocation = new GroupBox();
-	private RadioButton rbTop = new RadioButton();
-	private RadioButton rbBottom = new RadioButton();
-	private Label lblNumber = new Label();
-	private ComboBox cbNumber = new ComboBox();
+	private Label lblMeasure = new Label();
+	private TextBox txtMeasure = new TextBox();
+	private Label lblBeat = new Label();
+	private TextBox txtBeat = new TextBox();
 	private Label lblNotes = new Label();
 	private TextBox txtNotes = new TextBox();
 	private Button btnAdd = new Button();
-	private Button btnFillGaps = new Button();
 	private Button btnCancel = new Button();		
 
 	public EntryPoint() {
-		gbLocation.Size = new Size(170, 50);
-		gbLocation.Location = new Point(10, 10);
-		gbLocation.Text = "Location";
-		gbLocation.Controls.AddRange(new Control[] {
-			rbTop,
-			rbBottom});
+		lblMeasure.Size = new Size(60, 20);
+		lblMeasure.Location = new Point(10, 10);
+		lblMeasure.Text = "&Measure:";
 		
-		rbTop.Size = new Size(50, 20);
-		rbTop.Location = new Point(20, 20);
-		rbTop.Text = "&Top";
-		rbTop.Checked = true;
-		rbTop.CheckedChanged += new EventHandler(rbTop_CheckedChanged);
+		txtMeasure.Size = new Size(30, 50);
+		txtMeasure.Location = new Point(70, 10);
+		txtMeasure.Text = "1";
+		txtMeasure.Validated += new EventHandler(txtBeat_Validated);
 		
-		rbBottom.Size = new Size(60, 20);
-		rbBottom.Location = new Point(100, 20);
-		rbBottom.Text = "&Bottom";
+		lblBeat.Size = new Size(40, 20);
+		lblBeat.Location = new Point(110, 10);
+		lblBeat.Text = "&Beat:";
 		
-		lblNumber.Location = new Point(10, 80);
-		lblNumber.Text = "&Number:";
-		
-		cbNumber.Size = new Size(40, 50);
-		cbNumber.Location = new Point(140, 80);
-		cbNumber.Items.AddRange(TOP_RANGE);
-		cbNumber.SelectedIndex = 0;
-		cbNumber.Validated += new EventHandler(cbNumber_Validated);
+		txtBeat.Size = new Size(30, 50);
+		txtBeat.Location = new Point(150, 10);
+		txtBeat.Text = "1";
+		txtBeat.Validated += new EventHandler(txtBeat_Validated);
 		
 		lblNotes.Size = new Size(40, 60);
-		lblNotes.Location = new Point(10, 120);
+		lblNotes.Location = new Point(10, 50);
 		lblNotes.Text = "N&otes:";
 		
 		txtNotes.Multiline = true;
 		txtNotes.ScrollBars = ScrollBars.Vertical;
 		txtNotes.Size = new Size(120, 60);
-		txtNotes.Location = new Point(60, 120);
+		txtNotes.Location = new Point(60, 50);
 		txtNotes.Text = "[Section]";
 		
-		btnAdd.Location = new Point(10, 200);
+		btnAdd.Location = new Point(60, 130);
 		btnAdd.Text = "&Add";
 		btnAdd.Click += new EventHandler(btnAdd_Click);
-
-		btnFillGaps.Location = new Point(105, 200);
-		btnFillGaps.Text = "&Fill Gaps";
-		btnFillGaps.Click += new EventHandler(btnFillGaps_Click);
 
 		btnCancel.Click += new EventHandler(btnCancel_Click);
 		btnCancel.Size = new Size(0, 0);
 
-		Size = new Size(190, 240);
 		Controls.AddRange(new Control[] {
-			gbLocation,
-			lblNumber,
-			cbNumber,
+			lblMeasure,
+			txtMeasure,
+			lblBeat,
+			txtBeat,
 			lblNotes,
 			txtNotes,
 			btnAdd,
-			btnFillGaps,
 			btnCancel});
 	}
 	
@@ -95,20 +71,22 @@ public class EntryPoint : Form {
 	//
 	////////////////////////////////////////////////////////////////////////////////
 	
-	void rbTop_CheckedChanged(object sender, EventArgs e) {
-		if (rbTop.Checked) {
-			cbNumber.Items.Clear();
-			cbNumber.Items.AddRange(TOP_RANGE);
-		} else {
-			cbNumber.Items.Clear();
-			cbNumber.Items.AddRange(BOTTOM_RANGE);
+	void txtBeat_Validated(object sender, EventArgs e) {
+		int rulerNumber;
+		
+		try {
+			rulerNumber = Convert.ToInt32(txtBeat.Text);
+		} catch (Exception ex) {
+			MessageBox.Show("Invalid Ruler Number");
+			txtBeat.Focus();
+			return;
 		}
 		
-		Validate_cbNumber();
-	}
-	
-	void cbNumber_Validated(object sender, EventArgs e) {
-		Validate_cbNumber();
+		if ((true && (rulerNumber < 1 || rulerNumber > 12))
+			|| (false && (rulerNumber < 1 || rulerNumber > 16))) {
+			MessageBox.Show("Ruler Number out of range");
+			txtBeat.Focus();
+		}
 	}
 	
 	void btnAdd_Click(object sender, EventArgs e) {
@@ -136,38 +114,9 @@ public class EntryPoint : Form {
 		}
 		
 		Video.AddRuler(selectedVideoTracks[0], Common.vegas.Transport.CursorPosition,
-			rbTop.Checked, Convert.ToInt32(cbNumber.Text),
+			true, Convert.ToInt32(txtBeat.Text),
 			txtNotes.Text == "[Section]" ? "." : txtNotes.Text);
 			
-		Close();
-	}
-	
-	void btnFillGaps_Click(object sender, EventArgs e) {
-		List<VideoTrack> selectedVideoTracks = Common.TracksToVideoTracks(
-			Common.FindSelectedTracks(Common.VideoTracksToTracks(Video.FindVideoTracks(Common.vegas.Project)))
-		);
-		
-		List<TrackEvent> events = Common.TrackEventsToTrackEvents(selectedVideoTracks[0].Events);
-		
-		// there must be at least two events on the track to continue
-		if (events.Count < 2) {
-			MessageBox.Show("Please make sure there are at least two events on the track to continue",
-				Common.ADD_BEEP, MessageBoxButtons.OK, MessageBoxIcon.Error);
-			return;
-		}
-		
-		int eventsTrimmed = 0;
-		for (int i = 0; i < events.ToArray().Length - 1; i++) {
-			TrackEvent @event = events.ToArray()[i];
-			
-			if (@event.End < events.ToArray()[i + 1].Start) {
-				@event.End = events.ToArray()[i + 1].Start;
-				eventsTrimmed++;
-			}
-		}
-		
-		MessageBox.Show(eventsTrimmed + " out of " + events.Count +
-			" total events were trimmed", Common.ADD_BEEP);
 		Close();
 	}
 	
@@ -181,24 +130,6 @@ public class EntryPoint : Form {
 	//
 	////////////////////////////////////////////////////////////////////////////////
 	
-	private void Validate_cbNumber() {
-		int rulerNumber;
-		
-		try {
-			rulerNumber = Convert.ToInt32(cbNumber.Text);
-		} catch (Exception e) {
-			MessageBox.Show("Invalid Ruler Number");
-			cbNumber.Focus();
-			return;
-		}
-		
-		if ((rbTop.Checked && (rulerNumber < 1 || rulerNumber > 12))
-			|| (rbBottom.Checked && (rulerNumber < 1 || rulerNumber > 16))) {
-			MessageBox.Show("Ruler Number out of range");
-			cbNumber.Focus();
-		}
-	}
-	
     public void FromVegas(Vegas vegas) {
 		Common.vegas = vegas;
 
@@ -209,7 +140,7 @@ public class EntryPoint : Form {
 		AcceptButton = btnAdd;
 		CancelButton = btnCancel;
 		StartPosition = FormStartPosition.CenterParent;
-		Size = new Size(200, 260);
+		Size = new Size(200, 190);
 
 		List<VideoTrack> selectedVideoTracks = Common.TracksToVideoTracks(
 			Common.FindSelectedTracks(Common.VideoTracksToTracks(Video.FindVideoTracks(vegas.Project)))
