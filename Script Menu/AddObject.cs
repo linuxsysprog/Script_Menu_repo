f7a05d4b0c26f96e25d8249b9a5085d094b8b07a
Andrey Chislenko <linuxsysprog@yandex.com>
2011-12-30 03:55:49 +0000

    implemented AddObject GUI with stub event handlers
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@69 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/AddObject.cs
+++ Script Menu/AddObject.cs
@@ -10,45 +10,47 @@ using Sony.Vegas;
 using AddRulerNamespace;
 
 public class EntryPoint : Form {
-	private Label lblMeasure = new Label();
-	private TextBox txtMeasure = new TextBox();
-	private Label lblBeat = new Label();
-	private TextBox txtBeat = new TextBox();
-	private Label lblNotes = new Label();
-	private TextBox txtNotes = new TextBox();
+	private Label lblFrameSize = new Label();
+	private ComboBox cbFrameSize = new ComboBox();
+	private Label lblObject = new Label();
+	private ComboBox cbObject = new ComboBox();
+	private Label lblPreset = new Label();
+	private ComboBox cbPreset = new ComboBox();
 	private Button btnAdd = new Button();
 	private Button btnCancel = new Button();		
 
 	public EntryPoint() {
-		lblMeasure.Size = new Size(60, 20);
-		lblMeasure.Location = new Point(10, 10);
-		lblMeasure.Text = "&Measure:";
+		lblFrameSize.Size = new Size(70, 20);
+		lblFrameSize.Location = new Point(10, 10);
+		lblFrameSize.Text = "&Frame Size:";
 		
-		txtMeasure.Size = new Size(30, 50);
-		txtMeasure.Location = new Point(70, 10);
-		txtMeasure.Text = "1";
-		txtMeasure.Validated += new EventHandler(txtMeasure_Validated);
+		cbFrameSize.Size = new Size(200, 50);
+		cbFrameSize.Location = new Point(80, 10);
+		cbFrameSize.Items.AddRange(new object[] { "n/a" });
+		cbFrameSize.SelectedIndex = 0;
+		cbFrameSize.Validated += new EventHandler(cbFrameSize_Validated);
 		
-		lblBeat.Size = new Size(40, 20);
-		lblBeat.Location = new Point(110, 10);
-		lblBeat.Text = "&Beat:";
+		lblObject.Size = new Size(70, 20);
+		lblObject.Location = new Point(10, 50);
+		lblObject.Text = "&Object:";
 		
-		txtBeat.Size = new Size(30, 50);
-		txtBeat.Location = new Point(150, 10);
-		txtBeat.Text = "1";
-		txtBeat.Validated += new EventHandler(txtBeat_Validated);
+		cbObject.Size = new Size(200, 50);
+		cbObject.Location = new Point(80, 50);
+		cbObject.Items.AddRange(new object[] { "n/a" });
+		cbObject.SelectedIndex = 0;
+		cbObject.Validated += new EventHandler(cbFrameSize_Validated);
 		
-		lblNotes.Size = new Size(40, 60);
-		lblNotes.Location = new Point(10, 50);
-		lblNotes.Text = "N&otes:";
+		lblPreset.Size = new Size(70, 20);
+		lblPreset.Location = new Point(10, 90);
+		lblPreset.Text = "&Preset:";
 		
-		txtNotes.Multiline = true;
-		txtNotes.ScrollBars = ScrollBars.Vertical;
-		txtNotes.Size = new Size(120, 60);
-		txtNotes.Location = new Point(60, 50);
-		txtNotes.Text = "[Section]";
+		cbPreset.Size = new Size(200, 50);
+		cbPreset.Location = new Point(80, 90);
+		cbPreset.Items.AddRange(new object[] { "n/a" });
+		cbPreset.SelectedIndex = 0;
+		cbPreset.Validated += new EventHandler(cbFrameSize_Validated);
 		
-		btnAdd.Location = new Point(60, 130);
+		btnAdd.Location = new Point(110, 130);
 		btnAdd.Text = "&Add";
 		btnAdd.Click += new EventHandler(btnAdd_Click);
 
@@ -56,12 +58,12 @@ public class EntryPoint : Form {
 		btnCancel.Size = new Size(0, 0);
 
 		Controls.AddRange(new Control[] {
-			lblMeasure,
-			txtMeasure,
-			lblBeat,
-			txtBeat,
-			lblNotes,
-			txtNotes,
+			lblFrameSize,
+			cbFrameSize,
+			lblObject,
+			cbObject,
+			lblPreset,
+			cbPreset,
 			btnAdd,
 			btnCancel});
 	}
@@ -72,36 +74,21 @@ public class EntryPoint : Form {
 	//
 	////////////////////////////////////////////////////////////////////////////////
 	
-	void txtMeasure_Validated(object sender, EventArgs e) {
-		int measure;
+	void cbFrameSize_Validated(object sender, EventArgs e) {
+		// int measure;
 		
-		try {
-			measure = Convert.ToInt32(txtMeasure.Text);
-			if (measure < 1) {
-				throw new Exception("measure is less than one");
-			}
-		} catch (Exception ex) {
-			MessageBox.Show("Invalid Measure");
-			txtMeasure.Focus();
-		}
-	}
-	
-	void txtBeat_Validated(object sender, EventArgs e) {
-		int beat;
-		
-		try {
-			beat = Convert.ToInt32(txtBeat.Text);
-			if (beat < 1) {
-				throw new Exception("beat is less than one");
-			}
-		} catch (Exception ex) {
-			MessageBox.Show("Invalid Beat");
-			txtBeat.Focus();
-		}
+		// try {
+			// measure = Convert.ToInt32(cbFrameSize.Text);
+			// if (measure < 1) {
+				// throw new Exception("measure is less than one");
+			// }
+		// } catch (Exception ex) {
+			// MessageBox.Show("Invalid Measure");
+			// cbFrameSize.Focus();
+		// }
 	}
 	
 	void btnAdd_Click(object sender, EventArgs e) {
-		MessageBox.Show("Invaders!");
 		Close();
 	}
 	
@@ -125,7 +112,7 @@ public class EntryPoint : Form {
 		AcceptButton = btnAdd;
 		CancelButton = btnCancel;
 		StartPosition = FormStartPosition.CenterParent;
-		Size = new Size(200, 190);
+		Size = new Size(300, 190);
 
 		List<VideoTrack> selectedVideoTracks = Common.TracksToVideoTracks(
 			Common.FindSelectedTracks(Common.VideoTracksToTracks(Video.FindVideoTracks(vegas.Project)))
