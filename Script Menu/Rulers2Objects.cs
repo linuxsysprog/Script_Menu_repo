b96807a6a1030dbef306cf3c0c8bbd3696a8c397
Andrey Chislenko <linuxsysprog@yandex.com>
2012-02-05 05:27:47 +0000

    finished Rulers2Objects.cs
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@145 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/Rulers2Objects.cs
+++ Script Menu/Rulers2Objects.cs
@@ -19,6 +19,9 @@ public class EntryPoint {
 		List<VideoEvent> sourceEvents;
 		List<VideoEvent> targetEvents;
 		
+		const string frameSize = "320x240";
+		const string @object = "Measure";
+		
 		// get text media generator
 		PlugInNode plugIn = vegas.Generators.GetChildByName("Sony Text");
 		if (plugIn == null) {
@@ -40,7 +43,7 @@ public class EntryPoint {
 		// figure out measure range
 		List<int> measures = new List<int>();
 		foreach (Preset preset in presets) {
-			if (preset.FrameSize == "320x240" && preset.Object == "Measure") {
+			if (preset.FrameSize == frameSize && preset.Object == @object) {
 				try {
 					int n = Convert.ToInt32(preset.Value);
 					if (n < 1) {
@@ -124,90 +127,28 @@ public class EntryPoint {
 			}
 		}
 		
-		// check if there's enough measures
+		// check if there's enough measure presets
 		if (measures.Count < sourceEvents.Count) {
 			MessageBox.Show("Not enough Measure Presets (" + measures.Count + ")",
 				Common.RULERS_OBJECTS, MessageBoxButtons.OK, MessageBoxIcon.Error);
 			return;
 		}
 		
-		MessageBox.Show("Invaders!");
-		return;
-		
-		// find and insert events
-		int insertedEvents = 0;
-		foreach (VideoEvent @event in sourceEvents) {
-			// for event to qualify it should have at least one take that matches 
-			// ^(1|5|9|13) B pattern
-			bool eventOK = false;
-			foreach (Take take in @event.Takes) {
-				if (take.MediaStream == null ||
-						take.MediaStream.MediaType != MediaType.Video) {
-					continue;
-				}
-				
-				if (regex.Matches(take.Name).Count > 0) {
-					eventOK = true;
-					break;
-				}
-			}
-			if (!eventOK) {
-				continue;
-			}
-			
-			// create event
-			VideoEvent result = AddVideoEvent(targetTrack, @event);
-			if (result != null) {
-				insertedEvents++;
+		// insert measure objects
+		// Common.vegas.DebugClear();
+		{
+			int i = 0;
+			foreach (VideoEvent sourceEvent in sourceEvents) {
+				// create event
+				// Common.vegas.DebugOut(frameSize + " " + @object + " " + measures.ToArray()[i++]);
+				Common.AddTextEvent(Common.vegas, plugIn, targetTrack,
+					frameSize + " " + @object + " " + measures.ToArray()[i++],
+					sourceEvent.Start, Timecode.FromFrames(1));
 			}
 		}
 		
 		// report
-		MessageBox.Show("Inserted " + insertedEvents + " events", Common.RULERS_OBJECTS);
-	}
-	
-	// add a video event with a top ruler onto specified target video track.
-	// Copy position and take names from the source video event
-	private VideoEvent AddVideoEvent(VideoTrack tagretTrack, VideoEvent sourceEvent) {
-		// do not insert into the same slot more than once
-		List<TrackEvent> events = Common.FindEventsByPosition(tagretTrack, sourceEvent.Start);
-		if (events.Count > 0) {
-			return null;
-		}
-		
-		// insert event
-		VideoEvent videoEvent = tagretTrack.AddVideoEvent(sourceEvent.Start, Timecode.FromFrames(1));
-		
-		// copy take names across
-		foreach (Take take in sourceEvent.Takes) {
-			string path;
-			Media media;
-			
-			if (regex.Matches(take.Name).Count > 0) {
-				int rulerNumber = Convert.ToInt32(regex.Match(take.Name).Groups[1].Value);
-
-				// tweak ruler number
-				if (rulerNumber == 5) {
-					rulerNumber = 4;
-				} else if (rulerNumber == 9) {
-					rulerNumber = 7;
-				} else if (rulerNumber == 13) {
-					rulerNumber = 10;
-				}
-				
-				path = Common.vegas.InstallationDirectory + "\\Script Menu\\AddRuler.png\\" +
-					Common.LocationNumber2Basename(true, rulerNumber);
-				media = new Media(path);
-				(videoEvent.AddTake(media.GetVideoStreamByIndex(0), true)).Name =
-					rulerNumber + " T" + Common.SPACER;
-			} else {
-				path = Common.vegas.InstallationDirectory + "\\Script Menu\\AddRuler.png\\empty.png";
-				media = new Media(path);
-				(videoEvent.AddTake(media.GetVideoStreamByIndex(0), false)).Name = take.Name;
-			}
-		}
-		
-		return videoEvent;
+		MessageBox.Show("Inserted " + sourceEvents.Count + " events", Common.RULERS_OBJECTS);
 	}
 	
 }
