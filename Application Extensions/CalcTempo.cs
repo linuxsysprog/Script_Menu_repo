49328bf9bb1a0e760a3f3a83c4c35f60a633244b
Andrey Chislenko <linuxsysprog@yandex.com>
2012-02-12 04:39:16 +0000

    added Mute/Solo Audio Tracks GUI
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@155 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Application Extensions/CalcTempo.cs
+++ Application Extensions/CalcTempo.cs
@@ -37,7 +37,7 @@ public class CalcTempo : ICustomCommandModule {
 			calcTempoControl = new CalcTempoControl();
 			calcTempoView.Controls.Add(calcTempoControl);
 			
-			calcTempoView.DefaultFloatingSize = new Size(140, 170);
+			calcTempoView.DefaultFloatingSize = new Size(315, 215);
 			Common.vegas.LoadDockView(calcTempoView);
 		}
 	}
@@ -53,46 +53,100 @@ public class CalcTempo : ICustomCommandModule {
 }
 
 public class CalcTempoControl : UserControl {
+	const string PLAYING_TRACK = "Playing Track: ";
 
+	private GroupBox gbCalcTempo = new GroupBox();
 	private Label lblTempo = new Label();
 	public TextBox txtTempo = new TextBox();
 	public CheckBox chkDoubleTime = new CheckBox();
 	public CheckBox chkMonitorRegion = new CheckBox();
 	private Button btnCalcTempo = new Button();
 
+	private GroupBox gbMuteTracks= new GroupBox();
+	public CheckBox chkActivate= new CheckBox();
+	public CheckBox chkMuteAll= new CheckBox();
+	public CheckBox chkSoloAll= new CheckBox();
+	private Label lblPlayingTrack = new Label();
+	private Button btnSoloNext= new Button();
+
 	public CalcTempoControl() {
-	
-		lblTempo.Size = new Size(50, 20);
-		lblTempo.Location = new Point(10, 10);
-		lblTempo.Text = "&Tempo:";
-		
-		txtTempo.Size = new Size(60, 20);
-		txtTempo.Location = new Point(60, 10);
-		txtTempo.Text = "000.0000";
-		txtTempo.ReadOnly = true;
-		
-		chkDoubleTime.Size = new Size(100, 20);
-		chkDoubleTime.Location = new Point(10, 50);
-		chkDoubleTime.Text = "&Double Time";
-		chkDoubleTime.Click += new EventHandler(chkDoubleTime_Click);
-		
-		chkMonitorRegion.Size = new Size(150, 50);
-		chkMonitorRegion.Location = new Point(10, 60);
-		chkMonitorRegion.Text = "&Monitor Regions";
-		chkMonitorRegion.Click += new EventHandler(chkMonitorRegion_Click);
-		
-		btnCalcTempo.Location = new Point(30, 110);
-		btnCalcTempo.Text = "&Calculate";
-		btnCalcTempo.Click += new EventHandler(btnCalcTempo_Click);
-		
-		Size = new Size(1000, 1000);
-		Controls.AddRange(new Control[] {
+		gbCalcTempo.Size = new Size(135, 170);
+		gbCalcTempo.Location = new Point(10, 10);
+		gbCalcTempo.Text = "Calculate Tempo";
+		gbCalcTempo.Controls.AddRange(new Control[] {
 			lblTempo,
 			txtTempo,
 			chkDoubleTime,
 			chkMonitorRegion,
 			btnCalcTempo});
+			
+		lblTempo.Size = new Size(50, 20);
+		lblTempo.Location = new Point(10, 20);
+		lblTempo.Text = "&Tempo:";
+		
+		txtTempo.Size = new Size(60, 20);
+		txtTempo.Location = new Point(60, 20);
+		txtTempo.Text = "000.0000";
+		txtTempo.ReadOnly = true;
+		
+		chkDoubleTime.Size = new Size(100, 20);
+		chkDoubleTime.Location = new Point(10, 60);
+		chkDoubleTime.Text = "&Double Time";
+		chkDoubleTime.Click += new EventHandler(chkDoubleTime_Click);
+		
+		chkMonitorRegion.Size = new Size(120, 50);
+		chkMonitorRegion.Location = new Point(10, 70);
+		chkMonitorRegion.Text = "&Monitor Regions";
+		chkMonitorRegion.Click += new EventHandler(chkMonitorRegion_Click);
+		
+		btnCalcTempo.Location = new Point(30, 130);
+		btnCalcTempo.Text = "&Calculate";
+		btnCalcTempo.Click += new EventHandler(btnCalcTempo_Click);
+		
+		gbMuteTracks.Size = new Size(135, 170);
+		gbMuteTracks.Location = new Point(160, 10);
+		gbMuteTracks.Text = "Mute/Solo Tracks";
+		gbMuteTracks.Controls.AddRange(new Control[] {
+			chkActivate,
+			chkMuteAll,
+			chkSoloAll,
+			lblPlayingTrack,
+			btnSoloNext});
+			
+		chkActivate.Size = new Size(100, 20);
+		chkActivate.Location = new Point(10, 20);
+		chkActivate.Text = "&Activate";
+		chkActivate.Click += new EventHandler(chkActivate_Click);
+		
+		chkMuteAll.Size = new Size(100, 20);
+		chkMuteAll.Location = new Point(10, 40);
+		chkMuteAll.Text = "&Mute All";
+		chkMuteAll.Click += new EventHandler(chkMuteAll_Click);
+		
+		chkSoloAll.Size = new Size(100, 20);
+		chkSoloAll.Location = new Point(10, 60);
+		chkSoloAll.Text = "&Solo All";
+		chkSoloAll.Click += new EventHandler(chkSoloAll_Click);
+		
+		lblPlayingTrack.Size = new Size(100, 20);
+		lblPlayingTrack.Location = new Point(10, 90);
+		lblPlayingTrack.Text = PLAYING_TRACK;
+		
+		btnSoloNext.Location = new Point(30, 130);
+		btnSoloNext.Text = "Solo &Next";
+		btnSoloNext.Click += new EventHandler(btnSoloNext_Click);
+		
+		Size = new Size(1000, 1000);
+		Controls.AddRange(new Control[] {
+			gbCalcTempo,
+			gbMuteTracks});
 	}
+	
+	//
+	// Event handlers BEGIN
+	//
+	//
+	////////////////////////////////////////////////////////////////////////////////
 	
 	void btnCalcTempo_Click(object sender, EventArgs e) {
 		Selection selection = new Selection(Common.vegas.Transport.SelectionStart,
@@ -130,6 +184,24 @@ public class CalcTempoControl : UserControl {
 	void chkMonitorRegion_Click(object sender, EventArgs e) {
 			updateTempo();
 	}
+	
+	void chkActivate_Click(object sender, EventArgs e) {
+	}
+	
+	void chkMuteAll_Click(object sender, EventArgs e) {
+	}
+	
+	void chkSoloAll_Click(object sender, EventArgs e) {
+	}
+	
+	void btnSoloNext_Click(object sender, EventArgs e) {
+	}
+	
+	//
+	// Event handlers END
+	//
+	//
+	////////////////////////////////////////////////////////////////////////////////
 	
 	private double calcTempo(Timecode length) {
 		double tempo = 60.0 / (length.ToMilliseconds() / 1000.0);
@@ -174,7 +246,7 @@ public class CalcTempoControlTest : Form {
 	
 	public CalcTempoControlTest() {
 		Controls.Add(calcTempoControl);
-		Size = new Size(140, 170);
+		Size = new Size(315, 215);
 	}
 	
 	public static void Main() {
