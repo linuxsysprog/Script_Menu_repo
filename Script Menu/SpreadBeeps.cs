65009bbd8d9922e44bd1e2059636e2656692458b
Andrey Chislenko <linuxsysprog@yandex.com>
2012-04-21 03:49:38 +0000

    added two native events check; added GUI
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@218 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/SpreadBeeps.cs
+++ Script Menu/SpreadBeeps.cs
@@ -12,47 +12,92 @@ using System.Text.RegularExpressions;
 using Sony.Vegas;
 using AddRulerNamespace;
 
-public class EntryPoint {
+public class EntryPoint : Form {
+	private Label lblLabel = new Label();
+	private TextBox txtTextBox = new TextBox();
+	private Label lblLabel2 = new Label();
+	private Button btnOK = new Button();
+	private Button btnCancel = new Button();
+	
+	public EntryPoint() {
+		lblLabel.Size = new Size(245, 20);
+		lblLabel.Location = new Point(30, 20);
+		lblLabel.Text = "Spread out (bring closer together) the beeps by ";
+		
+		txtTextBox.Size = new Size(30, 20);
+		txtTextBox.Location = new Point(275, 20);
+		
+		lblLabel2.Size = new Size(20, 20);
+		lblLabel2.Location = new Point(310, 20);
+		lblLabel2.Text = "%";
+		
+		btnOK.Location = new Point(145, 60);
+		btnOK.Text = "&OK";
+		btnOK.Click += new EventHandler(btnOK_Click);
+
+		btnCancel.Click += new EventHandler(btnCancel_Click);
+		btnCancel.Size = new Size(0, 0);
+
+		Controls.AddRange(new Control[] {
+			lblLabel,
+			txtTextBox,
+			lblLabel2,
+			btnOK,
+			btnCancel});
+
+		Text = Common.SPREAD_BEEPS;
+		FormBorderStyle = FormBorderStyle.FixedDialog;
+		MaximizeBox = false;
+		MinimizeBox = false;
+		AcceptButton = btnOK;
+		CancelButton = btnCancel;
+		StartPosition = FormStartPosition.CenterParent;
+		Size = new Size(365, 120);
+	}
+
+	void btnOK_Click(object sender, EventArgs e) {
+		Close();
+	}
+	
+	void btnCancel_Click(object sender, EventArgs e) {
+		Close();
+	}
+	
     public void FromVegas(Vegas vegas) {
 		Common.vegas = vegas;
-		Track sourceTrack;
-		Track targetTrack;
-		List<TrackEvent> sourceEvents;
+		Common.vegas.DebugClear();
 		
 		Selection selection = new Selection(vegas.Transport.SelectionStart, vegas.Transport.SelectionLength);
 		selection.Normalize();
 		
 		// check for the user has selected exactly one audio track
-		// List<Track> tracks =
-			// Common.FindSelectedTracks(Common.AudioTracksToTracks(Audio.FindAudioTracks(Common.vegas.Project)));
 		List<Track> tracks =
-			Common.FindSelectedTracks(Common.vegas.Project.Tracks);
+			Common.FindSelectedTracks(Common.AudioTracksToTracks(Audio.FindAudioTracks(Common.vegas.Project)));
 		if (tracks.Count != 1) {
 			MessageBox.Show("Please make sure you have selected exactly one audio track",
 				Common.SPREAD_BEEPS, MessageBoxButtons.OK, MessageBoxIcon.Error);
 			return;
 		}
 		
-		Common.vegas.DebugClear();
-		List<TrackEvent> elmoList = Common.FindNativeEvents(Common.TrackEventsToTrackEvents(tracks[0].Events));
-		foreach (TrackEvent elmoEvent in elmoList) {
-			string fullName = Common.getFullName(Common.getTakeNames(elmoEvent));
-			Common.vegas.DebugOut("" + elmoEvent.Index + (fullName == "" ? "" : " (" + fullName + ")"));
+		// at least two native events should exist to continue
+		List<TrackEvent> events;
+		if (selection.SelectionLength == new Timecode()) {
+			// events = Common.TrackEventsToTrackEvents(tracks.Events);
+			events = Common.FindNativeEvents(Common.TrackEventsToTrackEvents(tracks[0].Events));
+		} else {
+			events = Common.FindNativeEvents(Common.FindEventsBySelection(tracks[0], selection));
 		}
 		
+		if (events.Count < 2) {
+			MessageBox.Show("Please make sure you have at least two (native) events selected to continue",
+				Common.SPREAD_BEEPS, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			return;
+		}
+		
+		ShowDialog();
 		return;
-		
-		// find events
-		List<TrackEvent>[] events = new List<TrackEvent>[2];
-		for (int i = 0; i < 2; i++) {
-			if (selection.SelectionLength == new Timecode()) {
-				events[i] = Common.TrackEventsToTrackEvents(tracks[i].Events);
-			} else {
-				events[i] = Common.FindEventsBySelection(tracks[i], selection);
-			}
-		}
 
-		// sort out which track and event collection is the source
+		/*// sort out which track and event collection is the source
 		// and which is the target
 		sourceTrack = tracks[0];
 		targetTrack = tracks[1];
@@ -97,7 +142,7 @@ public class EntryPoint {
 		}
 		
 		// report
-		MessageBox.Show("Split " + splitEvents + " events", Common.SPREAD_BEEPS);
+		MessageBox.Show("Split " + splitEvents + " events", Common.SPREAD_BEEPS);*/
 	}
 	
 }
