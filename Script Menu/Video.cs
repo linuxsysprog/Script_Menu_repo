b6feea1228ab7a0370da8b9e894d3dda8df43e82
Andrey Chislenko <linuxsysprog@yandex.com>
2013-06-21 11:37:23 +0000

    Added the embryo of TextGenerator.
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@268 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/Video.cs
+++ Script Menu/Video.cs
@@ -5,7 +5,11 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 using Sony.Vegas;
 
 namespace AddRulerNamespace
@@ -97,5 +101,119 @@ public class Video {
 	}
 	
 }
+
+public class TextGenerator {
+	private Bitmap asciiChartBitmap;
+	private char[][] asciiChart = new char[6][];
+
+	public TextGenerator(Bitmap asciiChartBitmap) {
+		if (null == asciiChartBitmap) {
+			throw new ArgumentException("invalid ascii chart");
+		}
+		
+		this.asciiChartBitmap = asciiChartBitmap;
+		initAsciiChart();
+	}
+	
+	private void initAsciiChart() {
+		asciiChart[0] = " !\"#$%&\'()*+,-./".ToCharArray();
+		asciiChart[1] = "0123456789:;<=>?".ToCharArray();
+		asciiChart[2] = "@ABCDEFGHIJKLMNO".ToCharArray();
+		asciiChart[3] = "PQRSTUVWXYZ[\\]^_".ToCharArray();
+		asciiChart[4] = "`abcdefghijklmno".ToCharArray();
+		asciiChart[5] = "pqrstuvwxyz{|}~ ".ToCharArray();
+	}
+	
+	private Coords getCharCoords(char c) {
+		for (int x = 0; x < asciiChart[0].Length; x++) {
+			for (int y = 0; y < asciiChart.Length; y++) {
+				if (c == asciiChart[y][x]) {
+					return new Coords(x, y);
+				}
+			}
+		}
+		
+		throw new ArgumentException("invalid char");
+	}
+	
+	public Bitmap getCharBitmap(char c) {
+		Coords coords = getCharCoords(c);
+		
+		// get dst pointer
+		Bitmap charBitmap = new Bitmap(8, 12, asciiChartBitmap.PixelFormat);
+		Rectangle charRect = new Rectangle(0, 0, charBitmap.Width, charBitmap.Height);
+		BitmapData charData = charBitmap.LockBits(charRect, ImageLockMode.WriteOnly, charBitmap.PixelFormat);
+		IntPtr dst = charData.Scan0;
+		
+		// get src pointer
+		Rectangle asciiChartRect = new Rectangle(0, 0, asciiChartBitmap.Width, asciiChartBitmap.Height);
+		BitmapData asciiChartData = asciiChartBitmap.LockBits(asciiChartRect, ImageLockMode.ReadOnly, asciiChartBitmap.PixelFormat);
+		IntPtr src = asciiChartData.Scan0;
+		
+		// copy data from ascii chart bitmap to array
+		int charStride = Math.Abs(asciiChartData.Stride) / asciiChart[0].Length;
+		byte[] ar = new byte[charStride * charBitmap.Height];
+		
+		long row = asciiChartData.Stride * charBitmap.Height * coords.y;
+		long column = charStride * coords.x;
+		long nextCharStride = row + column;
+		
+		for (int i = 0; i < charBitmap.Height; i++) {
+			Marshal.Copy(new IntPtr(src.ToInt64() + nextCharStride), ar, charStride * i, charStride);
+			nextCharStride += asciiChartData.Stride;
+		}
+		
+		// copy data from array to char bitmap
+		Marshal.Copy(ar, 0, dst, ar.Length);
+
+		// unlock and return
+		asciiChartBitmap.UnlockBits(asciiChartData);
+		charBitmap.UnlockBits(charData);
+		return charBitmap;
+	}
+	
+	/*
+	public void insertChar(char c , Bitmap frameBitmap, Coords coords) {
+		if (null == frameBitmap) {
+			throw new ArgumentException("invalid frame bitmap");
+		}
+		
+		if (coords.x < 0 || coords.x > 39 ||
+				coords.y < 0 || coords.y > 19) {
+			throw new ArgumentException("invalid coords");
+		}
+		
+		Bitmap charBitmap = getCharBitmap(c);
+		
+		// get src pointer
+		Bitmap charBitmap = new Bitmap(8, 12, asciiChartBitmap.PixelFormat);
+		Rectangle charRect = new Rectangle(0, 0, charBitmap.Width, charBitmap.Height);
+		BitmapData charData = charBitmap.LockBits(charRect, ImageLockMode.ReadOnly, charBitmap.PixelFormat);
+		IntPtr src = charData.Scan0;
+		
+		// get dst pointer
+		Rectangle frameBitmapRect = new Rectangle(0, 0, frameBitmap.Width, frameBitmap.Height);
+		BitmapData frameBitmapData = frameBitmap.LockBits(frameBitmapRect, ImageLockMode.WriteOnly, frameBitmap.PixelFormat);
+		IntPtr dst = frameBitmapData.Scan0;
+		
+	}
+	*/
+	
+}
+
+public struct Coords {
+	public int x;
+	public int y;
+	
+	public Coords(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public override string ToString() {
+		return "{x=" + x + ", y=" + y + "}";
+	}
+}
+
 }
 
