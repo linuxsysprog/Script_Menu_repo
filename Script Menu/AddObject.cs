b928085f08f05871f8cbaf936d1c2fed8e84902c
Andrey Chislenko <linuxsysprog@yandex.com>
2013-06-25 09:29:33 +0000

    Added history facility; added select on tab.
    git-svn-id: file:///cygdrive/c/tmp/nf1/Script_Menu_repo@281 59b06cf8-461b-aa4c-9181-dd8d1ffaba95

--- Script Menu/AddObject.cs
+++ Script Menu/AddObject.cs
@@ -9,74 +9,90 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
+using System.Xml;
 using Sony.Vegas;
 using AddRulerNamespace;
 
 public class EntryPoint : Form {
+	private Label lblProjectPath = new Label();
+	private MyTextBox txtProjectPath = new MyTextBox();
 	private Label lblFilename = new Label();
-	private TextBox txtFilename = new TextBox();
+	private MyTextBox txtFilename = new MyTextBox();
 	private Label lblNotes = new Label();
-	private TextBox txtNotes = new TextBox();
+	private MyTextBox txtNotes = new MyTextBox();
 	private Label lblTempo = new Label();
-	private TextBox txtTempo = new TextBox();
+	private MyTextBox txtTempo = new MyTextBox();
 	private Label lblRate = new Label();
-	private TextBox txtRate = new TextBox();
+	private MyTextBox txtRate = new MyTextBox();
 	private Label lblMeasure = new Label();
-	private TextBox txtMeasure = new TextBox();
+	private MyTextBox txtMeasure = new MyTextBox();
 	private Button btnAdd = new Button();
 	private Button btnCancel = new Button();
 	private Button btnClearAll = new Button();
 	
 	private List<VideoTrack> selectedVideoTracks;
+	private string configFilename = Common.vegas.InstallationDirectory + "\\Script Menu\\AddObject.cs.config";
+	private XmlDocument configXML = new XmlDocument();
+	private bool history = true;
+
 
 	public EntryPoint() {
+		lblProjectPath.Size = new Size(70, 20);
+		lblProjectPath.Location = new Point(10, 10);
+		lblProjectPath.Text = "&Project path:";
+		
+		txtProjectPath.Size = new Size(200, 50);
+		txtProjectPath.Location = new Point(80, 10);
+		
 		lblFilename.Size = new Size(70, 20);
-		lblFilename.Location = new Point(10, 10);
+		lblFilename.Location = new Point(10, 50);
 		lblFilename.Text = "&Filename:";
 		
 		txtFilename.Size = new Size(200, 50);
-		txtFilename.Location = new Point(80, 10);
+		txtFilename.Location = new Point(80, 50);
 		
 		lblNotes.Size = new Size(70, 20);
-		lblNotes.Location = new Point(10, 50);
+		lblNotes.Location = new Point(10, 90);
 		lblNotes.Text = "&Notes:";
 		
 		txtNotes.Size = new Size(200, 50);
-		txtNotes.Location = new Point(80, 50);
+		txtNotes.Location = new Point(80, 90);
 		
 		lblTempo.Size = new Size(70, 20);
-		lblTempo.Location = new Point(10, 90);
+		lblTempo.Location = new Point(10, 130);
 		lblTempo.Text = "&Tempo:";
 		
 		txtTempo.Size = new Size(200, 50);
-		txtTempo.Location = new Point(80, 90);
+		txtTempo.Location = new Point(80, 130);
 		
 		lblRate.Size = new Size(70, 20);
-		lblRate.Location = new Point(10, 130);
+		lblRate.Location = new Point(10, 170);
 		lblRate.Text = "&Rate:";
 		
 		txtRate.Size = new Size(200, 50);
-		txtRate.Location = new Point(80, 130);
+		txtRate.Location = new Point(80, 170);
 		
 		lblMeasure.Size = new Size(70, 20);
-		lblMeasure.Location = new Point(10, 170);
+		lblMeasure.Location = new Point(10, 210);
 		lblMeasure.Text = "&Measure:";
 		
 		txtMeasure.Size = new Size(200, 50);
-		txtMeasure.Location = new Point(80, 170);
+		txtMeasure.Location = new Point(80, 210);
 		
-		btnAdd.Location = new Point(60, 210);
+		btnAdd.Location = new Point(60, 250);
 		btnAdd.Text = "&Add";
 		btnAdd.Click += new EventHandler(btnAdd_Click);
 
 		btnCancel.Click += new EventHandler(btnCancel_Click);
 		btnCancel.Size = new Size(0, 0);
 
-		btnClearAll.Location = new Point(155, 210);
+		btnClearAll.Location = new Point(155, 250);
 		btnClearAll.Text = "&Clear All";
 		btnClearAll.Click += new EventHandler(btnClearAll_Click);
 
 		Controls.AddRange(new Control[] {
+			lblProjectPath,
+			txtProjectPath,
 			lblFilename,
 			txtFilename,
 			lblNotes,
@@ -90,6 +106,51 @@ public class EntryPoint : Form {
 			btnAdd,
 			btnCancel,
 			btnClearAll});
+			
+		// load config
+		try {
+			configXML.Load(configFilename);
+			
+			XmlNode projectPath = configXML.SelectSingleNode("/ScriptSettings/ProjectPath");
+			if (null == projectPath) {
+				throw new Exception("ProjectPath element not found");
+			}
+			txtProjectPath.Text = projectPath.InnerText;
+			
+			XmlNode filename = configXML.SelectSingleNode("/ScriptSettings/Filename");
+			if (null == filename) {
+				throw new Exception("Filename element not found");
+			}
+			txtFilename.Text = filename.InnerText;
+			
+			XmlNode notes = configXML.SelectSingleNode("/ScriptSettings/Notes");
+			if (null == notes) {
+				throw new Exception("Notes element not found");
+			}
+			txtNotes.Text = notes.InnerText;
+			
+			XmlNode tempo = configXML.SelectSingleNode("/ScriptSettings/Tempo");
+			if (null == tempo) {
+				throw new Exception("Tempo element not found");
+			}
+			txtTempo.Text = tempo.InnerText;
+			
+			XmlNode rate = configXML.SelectSingleNode("/ScriptSettings/Rate");
+			if (null == rate) {
+				throw new Exception("Rate element not found");
+			}
+			txtRate.Text = rate.InnerText;
+			
+			XmlNode measure = configXML.SelectSingleNode("/ScriptSettings/Measure");
+			if (null == measure) {
+				throw new Exception("Measure element not found");
+			}
+			txtMeasure.Text = measure.InnerText;
+		} catch (Exception ex) {
+			MessageBox.Show("Failed to load config file. History will be disabled: " + ex.Message,
+				Common.ADD_OBJECT, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			history = false;
+		}
 	}
 	
 	//
@@ -99,6 +160,22 @@ public class EntryPoint : Form {
 	////////////////////////////////////////////////////////////////////////////////
 	
 	void btnAdd_Click(object sender, EventArgs e) {
+		if (history) {
+			try {
+				configXML.SelectSingleNode("/ScriptSettings/ProjectPath").InnerText = txtProjectPath.Text;
+				configXML.SelectSingleNode("/ScriptSettings/Filename").InnerText = txtFilename.Text;
+				configXML.SelectSingleNode("/ScriptSettings/Notes").InnerText = txtNotes.Text;
+				configXML.SelectSingleNode("/ScriptSettings/Tempo").InnerText = txtTempo.Text;
+				configXML.SelectSingleNode("/ScriptSettings/Rate").InnerText = txtRate.Text;
+				configXML.SelectSingleNode("/ScriptSettings/Measure").InnerText = txtMeasure.Text;
+
+				configXML.Save(configFilename);
+			} catch (Exception ex) {
+				MessageBox.Show("Failed to save config file: " + ex.Message,
+					Common.ADD_OBJECT, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		
 		Close();
 	}
 	
@@ -107,13 +184,14 @@ public class EntryPoint : Form {
 	}
 	
 	void btnClearAll_Click(object sender, EventArgs e) {
+		txtProjectPath.Text = "";
 		txtFilename.Text = "";
 		txtNotes.Text = "";
 		txtTempo.Text = "";
 		txtRate.Text = "";
 		txtMeasure.Text = "";
 		
-		txtFilename.Focus();
+		txtProjectPath.Focus();
 	}
 	
 	//
@@ -133,7 +211,7 @@ public class EntryPoint : Form {
 		AcceptButton = btnAdd;
 		CancelButton = btnCancel;
 		StartPosition = FormStartPosition.CenterParent;
-		Size = new Size(300, 270);
+		Size = new Size(300, 310);
 
 		// get the video track to work on
 		selectedVideoTracks = Common.TracksToVideoTracks(
@@ -150,5 +228,38 @@ public class EntryPoint : Form {
 		ShowDialog();
 	}
 	
+}
+
+// Courtesy of http://stackoverflow.com/questions/97459/automatically-select-all-text-on-focus-in-winforms-textbox
+public class MyTextBox : System.Windows.Forms.TextBox
+{
+    private bool _focused;
+
+    protected override void OnEnter(EventArgs e)
+    {
+        base.OnEnter(e);
+        if (MouseButtons == MouseButtons.None)
+        {
+            SelectAll();
+            _focused = true;
+        }
+    }
+
+    protected override void OnLeave(EventArgs e)
+    {
+        base.OnLeave(e);
+        _focused = false;
+    }
+
+    protected override void OnMouseUp(MouseEventArgs mevent)
+    {
+        base.OnMouseUp(mevent);
+        if (!_focused)
+        {
+            if (SelectionLength == 0)
+                SelectAll();
+            _focused = true;
+        }
+    }
 }
 
